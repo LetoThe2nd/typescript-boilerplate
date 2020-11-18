4e7809dacdee9b460dec84c2eddf682baccc4854
Josef Holzmayr <holzmayr@rsi-elektrotechnik.de>
2020-11-18 14:52:09 +0000

    clean up add/index

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,7 @@
-import { simpleAdd } from "../src/add";
+import { simpleAdd } from "../src/index";
 
 describe("This is a simple test", () => {
     test("Check the simpleAdd function", () => {
         expect(simpleAdd(1, 1)).toEqual(2);
-    });
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { simpleAdd } from "./add";
-
-const f = "foo" + simpleAdd(1, 2).toFixed(0) + "moo";
-
-console.log({f});
+export const simpleAdd = (a: number, b: number) => {
+	return a + b;
+};
