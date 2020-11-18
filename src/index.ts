277dba502b324c1bf7c09ac4871fcd4b468c4cfb
Josef Holzmayr <holzmayr@rsi-elektrotechnik.de>
2020-11-18 11:25:02 +0000

    add webpack

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-const f = "foo" + "3";
+import { simpleAdd } from "./add";
+
+const f = "foo" + simpleAdd(1, 2).toFixed(0) + "moo";
 
 console.log({f});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require( 'path' );
+
+module.exports = {
+    // bundling mode
+    mode: 'development',
+
+    // entry files
+	entry: './src/index.ts',
+	
+    // output bundles (location)
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'index.js',
+        devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+	},
+	devtool: "source-map",
+
+    // file resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
